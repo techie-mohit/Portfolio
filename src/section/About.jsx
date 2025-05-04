4252eaf7f73f51c0ed418cdebdcfea205a198e2b
techie-mohit <mohitkeshari2000@gmail.com>
2025-05-04 22:55:44 +0530

    portfolio

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -13,9 +13,9 @@ const About = () => {
           <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
         </div>
         <div className="border border-white bg-gray-900 backdrop-blur-md  p-8 rounded-xl">
-            <p className="mt-4 text-gray-400 font-bold"> I am a full-stack developer with a passion for creating dynamic and responsive web applications. I have a strong foundation in computer science and a keen interest in web development. I have experience in building dynamic and responsive web applications using modern technologies.
+            <p className="mt-4 text-gray-400 font-bold"> I am a full-stack developer with a passion for creating dynamic and responsive web applications.
             Skilled in both front-end and back-end development, I specialize in the MERN stack and other
-            modern technologies to create seamless user experiences and efficient solutions. I am a passionate software engineer with a strong foundation in computer science and a keen interest in web development. I have experience in building dynamic and responsive web applications using modern technologies.</p>
+            modern technologies . I am a passionate software engineer with a strong foundation in computer science and a keen interest in web development.</p>
         </div>
 
         
